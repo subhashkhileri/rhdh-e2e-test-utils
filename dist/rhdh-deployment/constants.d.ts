298a565da3768a281f37ec3b286a6f4347dd8b2f
Subhash Khileri <skhileri@redhat.com>
2025-11-30 14:58:05 +0530

    rhdh e2e deployment 2

---
+++ dist/rhdh-deployment/constants.d.ts
@@ -0,0 +1,13 @@
+export declare const DEFAULT_CONFIG_PATHS: {
+    appConfig: string;
+    secrets: string;
+    dynamicPlugins: string;
+    helm: {
+        valueFile: string;
+    };
+    operator: {
+        subscription: string;
+    };
+};
+export declare const CHART_URL = "oci://quay.io/rhdh/chart";
+//# sourceMappingURL=constants.d.ts.map
---
+++ src/rhdh-deployment/constants.ts
@@ -0,0 +1,33 @@
+import path from "path";
+
+// Navigate from dist/rhdh-deployment/ to package root
+const PACKAGE_ROOT = path.resolve(import.meta.dirname, "../..");
+
+export const DEFAULT_CONFIG_PATHS = {
+  appConfig: path.join(
+    PACKAGE_ROOT,
+    "src/rhdh-deployment/config/app-config-rhdh.yaml",
+  ),
+  secrets: path.join(
+    PACKAGE_ROOT,
+    "src/rhdh-deployment/config/rhdh-secrets.yaml",
+  ),
+  dynamicPlugins: path.join(
+    PACKAGE_ROOT,
+    "src/rhdh-deployment/config/dynamic-plugins.yaml",
+  ),
+  helm: {
+    valueFile: path.join(
+      PACKAGE_ROOT,
+      "src/rhdh-deployment/helm/value_file.yaml",
+    ),
+  },
+  operator: {
+    subscription: path.join(
+      PACKAGE_ROOT,
+      "src/rhdh-deployment/operator/subscription.yaml",
+    ),
+  },
+};
+
+export const CHART_URL = "oci://quay.io/rhdh/chart";
